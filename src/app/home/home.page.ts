904ab664233c7964c75885c64674638b46b286d7
Jagdish Chopde <jagdishchopde@outlook.com>
2021-03-29 22:06:30 +0530

    cap android project added.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { WebIntent } from '@ionic-native/web-intent/ngx';
+import { AlertController } from '@ionic/angular';
+
 import { Plugins } from '@capacitor/core';
 
 const { Storage } = Plugins;
@@ -12,7 +15,7 @@ export class HomePage implements OnInit {
   dark = false;
   accentColor = 'primary';
 
-  constructor() {
+  constructor(private webIntent: WebIntent, public alertController: AlertController) {
     try {
       // const prefersColor = window.matchMedia('(prefers-color-scheme: dark)');
       // this.dark = prefersColor.matches;
@@ -92,4 +95,89 @@ export class HomePage implements OnInit {
       console.log(error);
     }
   }
+
+  // payViaUPI() {
+  //   try {
+  //     let guid = Math.floor(Math.random() * 16).toString(16).toUpperCase();
+  //     console.log(guid);
+
+  //     const options = {
+  //       action: this.webIntent.ACTION_VIEW,
+  //       url: 'upi://pay?pa=pychopde2011@okicici&pn=Pandurang Chopde&tid=' + guid + '&am=12&cu=INR&tn=App Payment&url=https://milan-powar.web.app/home',
+  //     }
+
+  //     this.webIntent.startActivity(options).then(
+  //       onSuccess => {
+  //         console.log('Success', onSuccess);
+  //         this.presentAlert('Success', onSuccess);
+  //     }, onError => {
+  //       console.log('Error', onError);
+  //       this.presentAlert('Error Fail', onError);
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //     this.presentAlert('Error', error);
+  //   }
+  // }
+
+
+  async presentAlert(title, msg) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: title,
+      subHeader: 'Subtitle',
+      message: msg,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+
+  payWithUPI() {
+    const tid = this.getRandomString();
+    const orderId = this.getRandomString();
+    const totalPrice = 1.00;
+    const UPI_ID = 'sandeepchopde03@oksbi';
+    const UPI_NAME = 'Sandeep Chopde';
+    const UPI_TXN_NOTE = 'TEST TXN';
+    // tslint:disable-next-line: max-line-length
+    let uri = `upi://pay?pa=${UPI_ID}&pn=${UPI_NAME}&tid=${tid}&am=${totalPrice}&cu=INR&tn=${UPI_TXN_NOTE}&tr=${orderId}`;
+    uri = uri.replace(' ', '+');
+    (window as any).plugins.intentShim.startActivityForResult(
+      {
+        action: this.webIntent.ACTION_VIEW,
+        url: uri,
+        requestCode: 1
+      }, intent => {
+        if (intent.extras.requestCode === 1 &&
+            intent.extras.resultCode === (window as any).plugins.intentShim.RESULT_OK &&
+            intent.extras.Status &&
+            (((intent.extras.Status as string).toLowerCase()) === ('success'))) {
+          this.paymentSuccess(orderId, 'UPI');
+        } else {
+          this.presentAlert('Error Fail', 'payment failed');
+          alert('payment failed');
+        }
+      }, err => {
+        alert('error ' + err);
+      });
+  }
+
+  getRandomString() {
+    const len = 10;
+    const arr = '1234567890asdfghjklqwertyuiopzxcvbnmASDFGHJKLQWERTYUIOPZXCVBNM';
+    let ans = '';
+    for (let i = len; i > 0; i--) {
+        ans += arr[Math.floor(Math.random() * arr.length)];
+    }
+    return ans;
+  }
+
+  paymentSuccess(orderId: string, paymentMethod: string) {
+    this.presentAlert('Success', `Payment successful Order Id ${orderId} payment method ${paymentMethod}`);
+    alert(`Payment successful Order Id ${orderId} payment method ${paymentMethod}`);
+  }
+
+
 }
